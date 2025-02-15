4c5d5e1f326de484313b13e8a8d3aaa3bec66793
Shandel <shandelvm16@gmail.com>
2025-02-15 17:54:47 -0300

    Update db_stardust (2).sql

--- db_stardust (2).sql
+++ db_stardust (2).sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `db_stardust`
 --
-CREATE DATABASE IF NOT EXISTS `db_stardust` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS `db_stardust` DEFAULT CHARACTER SET utf8mb4;
 USE `db_stardust`;
 
 -- --------------------------------------------------------
